cb52217432faceb73f033d2988a83cf872b35db7
Tamer <tamer.assaliya@gmail.com>
2019-02-12 15:03:22 +0200

    FCommit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world !");
+		JOptionPane.showMessageDialog(null, "hello world !");
+	}
+
+}
